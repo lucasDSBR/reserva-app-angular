6c19ef16d4c0cccf12653d17ad33ef9460707cbc
lucas <lucasmaciel6690@gmail.com>
2022-07-12 22:02:05 -0300

    Feature: Criando componentes

---
+++ app/src/app/pages/reservations-item/reservations-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservations-item',
+  templateUrl: './reservations-item.component.html',
+  styleUrls: ['./reservations-item.component.css']
+})
+export class ReservationsItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
